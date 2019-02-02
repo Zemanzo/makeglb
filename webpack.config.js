89e57721a49dcf05ddca048fb6f61d7c7827cadc
Mark Kurt <markkurt@fastmail.fm>
2019-02-01 20:01:01 -0600

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path')
 const webpack = require('webpack')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'makeglb.js',
-    library: 'MakeGLB',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'sample'),
-    hot: true,
-    port: 3000
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+module.exports = (env, argv) => {
+  const debug = argv.mode !== 'production'
+
+  const plugins = []
+  if (debug) {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'makeglb.js',
+      library: 'MakeGLB',
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'sample'),
+      hot: true,
+      port: 3000
+    },
+    plugins: plugins
+  }
 }
